b1db4a15970c54fcf735e23e4ec747c69dccb17e
Felipe-Gs <felipegs2905@gmail.com>
2023-02-13 10:49:59 -0300

    feat: adicionado context em volta da aplicação

--- mobile/App.js
+++ mobile/App.js
@@ -2,13 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./Routes";
 
 import { Provider as PaperProvider } from "react-native-paper";
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <PaperProvider>
-        <AppRoutes />
-      </PaperProvider>
+      <AuthContextProvider>
+        <PaperProvider>
+          <AppRoutes />
+        </PaperProvider>
+      </AuthContextProvider>
     </NavigationContainer>
   );
 }
--- mobile/src/pages/Home.js
+++ mobile/src/pages/Home.js
@@ -1,10 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Button } from "react-native-paper";
+
+import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
+  const { navigate } = useNavigation();
   return (
-    <View>
+    <View style={{ marginTop: 100 }}>
       <Text>Home</Text>
+      <Button mode="outlined" onPress={() => navigate("Login")}>
+        Ir para login
+      </Button>
     </View>
   );
 }
