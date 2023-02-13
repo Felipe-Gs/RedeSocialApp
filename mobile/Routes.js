268498692e4d91a00810d2f413fc55aae7b7f0a2
Felipe-Gs <felipegs2905@gmail.com>
2023-02-13 10:35:03 -0300

    feat: criado arquivo gerenciador de rotas

---
+++ mobile/Routes.js
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+import Home from "./src/pages/Home";
+import Login from "./src/pages/Login";
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+      <Screen name="Home" component={Home} />
+      <Screen name="Login" component={Login} />
+    </Navigator>
+  );
+}
