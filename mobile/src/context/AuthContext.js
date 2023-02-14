5a18b7ceffde776c45d555f16baa45c816925343
Felipe-Gs <felipegs2905@gmail.com>
2023-02-13 21:34:23 -0300

    feat: criado funções de cadastro e login

--- mobile/src/context/AuthContext.js
+++ mobile/src/context/AuthContext.js
@@ -1,17 +1,66 @@
 import { View, Text } from "react-native";
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import api from "../axios/api";
 
 export const AuthContext = createContext({});
 
 export function AuthContextProvider({ children }) {
-  const teste = () => {
-    console.log("ola mundo!");
+  // funções login
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [usuario, setUsuario] = useState();
+  const [error, setError] = useState("");
+  const handleLogin = async () => {
+    try {
+      if (email === "" || password === "") {
+        setError("Insira os dados corretamente");
+        return;
+      }
+      const response = await api.post("/login", {
+        email,
+        password,
+      });
+      setUsuario(response.data.usuario);
+      console.log(usuario);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // funcoes login
+
+  // funcoes cadastro
+  const [name, setName] = useState("");
+  const handleCadastro = async () => {
+    try {
+      if (email === "" || password === "" || name === "") {
+        setError("Insira os dados corretamente");
+        return;
+      }
+      const response = await api.post("/cadastrar", {
+        name,
+        email,
+        password,
+      });
+      alert(response.data.message);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <AuthContext.Provider
       value={{
-        teste,
+        handleLogin,
+        usuario,
+        email,
+        password,
+        error,
+        setError,
+        setPassword,
+        setEmail,
+        name,
+        setName,
+        handleCadastro,
       }}
     >
       {children}
