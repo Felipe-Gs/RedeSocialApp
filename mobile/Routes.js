aac35e0c3063cca01e06d0497cb51c3b5a038b06
Felipe-Gs <felipegs2905@gmail.com>
2023-02-13 21:32:21 -0300

    feat: adicionado novas rotas

--- mobile/Routes.js
+++ mobile/Routes.js
@@ -4,12 +4,23 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 import Home from "./src/pages/Home";
 import Login from "./src/pages/Login";
+import Cadastro from "./src/pages/Cadastro";
+import PostsGerais from "./src/pages/PostsGerais";
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: "black" },
+        transparentCard: true,
+      }}
+      initialRouteName="Home"
+    >
       <Screen name="Home" component={Home} />
       <Screen name="Login" component={Login} />
+      <Screen name="Cadastro" component={Cadastro} />
+      <Screen name="PostsGerais" component={PostsGerais} />
     </Navigator>
   );
 }
